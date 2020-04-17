5b0dcb08d702ae64a8bf82591e338a6bcb60f414
avk <alvanklaveren@gmaill.com>
2020-04-17 16:51:14 +0200

    minor change

--- src/main/java/com/alvanklaveren/rest/MessageController.java
+++ src/main/java/com/alvanklaveren/rest/MessageController.java
@@ -5,6 +5,7 @@ import com.alvanklaveren.usecase.MessageUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class MessageController {
     @Autowired
     private MessageUseCase messageUseCase;
 
+    @CrossOrigin
     @RequestMapping("/homepage")
     public ResponseEntity<List<MessageDTO>> getHomePageMessages() {
 
