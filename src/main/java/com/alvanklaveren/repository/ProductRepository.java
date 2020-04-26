4db9eaa115fb3a693bd1580f1136887968022b97
avk <alvanklaveren@gmaill.com>
2020-04-26 19:21:52 +0200

    sort by rating now only retrieves ONE of the ratings

--- src/main/java/com/alvanklaveren/repository/ProductRepository.java
+++ src/main/java/com/alvanklaveren/repository/ProductRepository.java
@@ -13,17 +13,26 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> getByGameConsole_CodeAndProductType_Code(Integer codeGameConsole, Integer codeProductType, Pageable pageable);
 
-    @Query("select p from Product p join ProductRating pr on pr.product = p and pr.rating > 0 " +
-            "where p.gameConsole.code = :codeGameConsole " +
+    @Query("select p from Product p " +
+            "join ProductRating pr on pr.product = p " +
+            "where pr.code = (select min(pr2.code) from ProductRating pr2 where pr2.product = p and pr2.rating > 0) " +
+            "and p.gameConsole.code = :codeGameConsole " +
             "order by pr.rating asc")
     List<Product> getByGameConsole_CodeByRating(Integer codeGameConsole, Pageable pageable);
 
-    @Query("select p from Product p join ProductRating pr on pr.product = p and pr.rating > 0 " +
-            "where p.gameConsole.code = :codeGameConsole and p.productType.code = :codeProductType " +
+    @Query("select p from Product p " +
+            "join ProductRating pr on pr.product = p " +
+            "where pr.code = (select min(pr2.code) from ProductRating pr2 where pr2.product = p and pr2.rating > 0) " +
+            "and p.gameConsole.code = :codeGameConsole and p.productType.code = :codeProductType  " +
             "order by pr.rating asc")
     List<Product> getByGameConsole_CodeAndProductType_CodeByRating(Integer codeGameConsole, Integer codeProductType, Pageable pageable);
 
-    @Query("select p from Product p join ProductRating pr on pr.product = p and pr.rating > 0 order by pr.rating asc")
+    @Query("select p from Product p " +
+            "join ProductRating pr on pr.product = p " +
+            "where pr.code = (select min(pr2.code) from ProductRating pr2 where pr2.product = p and pr2.rating > 0) " +
+            "order by pr.rating asc")
+//    WHERE pk.expiryDate = (SELECT MAX(ppk.expiryDate) FROM Pack ppk where ppk.product = pk.product) AND
+//    pk.expiryDate BETWEEN :start AND :end
     List<Product> getAllProductsByRating(Pageable pageable);
 
     @Query("select p from Product p")
